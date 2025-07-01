6e93ea2878d120a683cc41f191292cc2adac9ba4
martinperezcanada <148330505+martinperezcanada@users.noreply.github.com>
2025-07-01 11:08:23 +0200

    Configurar CORS para frontend Vercel

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://frontend-juego-calamar.vercel.app'], // pon aquí la URL de tu frontend desplegado
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
-  
 }
+
 bootstrap();
